c1560d5b739210c6c57b82cc9200d103248edab9
suxing.zhang <suxing.zhang@pousheng.com>
2021-02-01 21:35:29 +0800

    test case update

---
+++ omen-sample/src/test/java/com/omen/learning/sample/controller/JacksonControllerTest.java
@@ -0,0 +1,14 @@
+package com.omen.learning.sample.controller;
+
+import org.junit.Test;
+
+public class JacksonControllerTest {
+    @Test
+    public void testJackSon() throws Exception {
+    }
+
+    @Test
+    public void testJackSon1() throws Exception {
+    }
+
+}
