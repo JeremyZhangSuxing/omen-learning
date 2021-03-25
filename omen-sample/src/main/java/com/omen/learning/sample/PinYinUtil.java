ca77beca5f4f55f5c0381b903f097bbf73d95bfc
knight <suxing.zhang3@wework.com>
2021-03-25 09:58:48 +0800

    中英文汉子首字母排序

--- omen-sample/src/main/java/com/omen/learning/sample/PinYinUtil.java
+++ omen-sample/src/main/java/com/omen/learning/sample/PinYinUtil.java
@@ -8,22 +8,19 @@ import net.sourceforge.pinyin4j.format.HanyuPinyinVCharType;
 import net.sourceforge.pinyin4j.format.exception.BadHanyuPinyinOutputFormatCombination;
 
 public class PinYinUtil {
-    public static void main(String[] args) throws BadHanyuPinyinOutputFormatCombination {
-        System.out.println(getPinyin("中国红123"));
-    }
 
     /**
      * @param key (字符串 汉字)
      * @return 汉字转拼音 其它字符不变
      */
     public static String getPinyin(String key) throws BadHanyuPinyinOutputFormatCombination {
-        HanyuPinyinOutputFormat formart = new HanyuPinyinOutputFormat();
-        formart.setCaseType(HanyuPinyinCaseType.LOWERCASE);
-        formart.setToneType(HanyuPinyinToneType.WITHOUT_TONE);
-        formart.setVCharType(HanyuPinyinVCharType.WITH_V);
+        HanyuPinyinOutputFormat letter = new HanyuPinyinOutputFormat();
+        letter.setCaseType(HanyuPinyinCaseType.LOWERCASE);
+        letter.setToneType(HanyuPinyinToneType.WITHOUT_TONE);
+        letter.setVCharType(HanyuPinyinVCharType.WITH_V);
         char[] arrays = key.trim().toCharArray();
         if (Character.toString(arrays[0]).matches("[\\u4e00-\\u9fa5]")) {
-            String[] temp = PinyinHelper.toHanyuPinyinStringArray(arrays[0], formart);
+            String[] temp = PinyinHelper.toHanyuPinyinStringArray(arrays[0], letter);
             return temp[0].substring(0, 1);
         }
         return String.valueOf(arrays[0]);
