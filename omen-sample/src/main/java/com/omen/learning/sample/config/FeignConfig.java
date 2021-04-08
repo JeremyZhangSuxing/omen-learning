d5fe81a65b243e587747260a19a484d9256c49bc
knight <suxing.zhang3@wework.com>
2021-04-08 11:27:26 +0800

    feign client 客户端传文件配置

--- omen-sample/src/main/java/com/omen/learning/sample/config/FeignConfig.java
+++ omen-sample/src/main/java/com/omen/learning/sample/config/FeignConfig.java
@@ -1,6 +1,11 @@
 package com.omen.learning.sample.config;
 
 import feign.Logger;
+import feign.codec.Encoder;
+import feign.form.spring.SpringFormEncoder;
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,5 +21,15 @@ public class FeignConfig {
         return Logger.Level.FULL;
     }
 
+    @Autowired
+    private ObjectFactory<HttpMessageConverters> messageConverters;
+
+    /**
+     * 文件上传时  指定文件解码器
+     */
+    @Bean
+    public Encoder feignFormEncoder(){
+        return new SpringFormEncoder();
+    }
 
 }
