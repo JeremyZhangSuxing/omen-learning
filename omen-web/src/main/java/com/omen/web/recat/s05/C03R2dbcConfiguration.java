adfb46448f22898be7615da919ba90160a6120d4
knight <suxing.zhang3@wework.com>
2021-05-26 14:06:29 +0800

    引入 R2DBC

---
+++ omen-web/src/main/java/com/omen/web/recat/s05/C03R2dbcConfiguration.java
@@ -0,0 +1,37 @@
+package com.omen.web.recat.s05;
+
+import io.r2dbc.spi.ConnectionFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.data.r2dbc.connectionfactory.R2dbcTransactionManager;
+import org.springframework.data.r2dbc.connectionfactory.init.CompositeDatabasePopulator;
+import org.springframework.data.r2dbc.connectionfactory.init.ConnectionFactoryInitializer;
+import org.springframework.data.r2dbc.connectionfactory.init.ResourceDatabasePopulator;
+import org.springframework.transaction.ReactiveTransactionManager;
+import org.springframework.transaction.reactive.TransactionalOperator;
+
+@Configuration
+//@EnableTransactionManagement
+public class C03R2dbcConfiguration {
+    @Bean
+    public ConnectionFactoryInitializer connectionFactoryInitializer(@Qualifier("connectionFactory") ConnectionFactory connectionFactory) {
+        ConnectionFactoryInitializer initializer = new ConnectionFactoryInitializer();
+        initializer.setConnectionFactory(connectionFactory);
+        CompositeDatabasePopulator populator = new CompositeDatabasePopulator();
+        populator.addPopulators(new ResourceDatabasePopulator(new ClassPathResource("schema.sql")));
+        initializer.setDatabasePopulator(populator);
+        return initializer;
+    }
+
+    @Bean
+    public ReactiveTransactionManager transactionManager(@Qualifier("connectionFactory") ConnectionFactory connectionFactory) {
+        return (new R2dbcTransactionManager(connectionFactory));
+    }
+
+    @Bean
+    public TransactionalOperator transactionalOperator(ReactiveTransactionManager transactionManager) {
+        return TransactionalOperator.create(transactionManager);
+    }
+}
