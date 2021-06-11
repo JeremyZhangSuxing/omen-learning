1d44c668d4a3d0c44b526c54e51b84a5687dbe99
knight <suxing.zhang3@wework.com>
2021-06-11 16:10:28 +0800

    code update

--- omen-sample/src/main/java/com/omen/learning/sample/Application.java
+++ omen-sample/src/main/java/com/omen/learning/sample/Application.java
@@ -2,6 +2,7 @@ package com.omen.learning.sample;
 
 import com.omen.learning.bean.test.annotattion.EnableBeanRegistry;
 import com.omen.learning.common.annotation.EnableTokenValidate;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
@@ -12,12 +13,13 @@ import org.springframework.scheduling.annotation.EnableAsync;
  * @author : Knight
  * @date : 2020/11/8 10:57 上午
  */
-@SpringBootApplication
 @EnableTokenValidate
 @EnableBeanRegistry
 @EnableCaching
 @EnableFeignClients
 @EnableAsync
+@MapperScan("com.omen.learning.sample.mybatis.mapper")
+@SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
