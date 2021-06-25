f34a802184a2e671da6dbf30a150ec6016252ef0
knight <suxing.zhang3@wework.com>
2021-06-25 14:58:35 +0800

    事件 常规使用

--- omen-sample/src/main/java/com/omen/learning/sample/service/EventService.java
+++ omen-sample/src/main/java/com/omen/learning/sample/service/EventService.java
@@ -2,6 +2,7 @@ package com.omen.learning.sample.service;
 
 import com.omen.learning.common.entity.BillDTO;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Order;
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
@@ -20,10 +21,26 @@ public class EventService {
 
     /**
      * 处理jIT发货单
+     * 一般事件发布为单线程处理，可以用以代码重构时不改变原代码结构挥着避免循环依赖等case
+     * 可以通过 {@link org.junit.jupiter.api.Order} 指定同一事件消费次序
      */
     @Async
     @EventListener(condition = "#billDTO.billType.equalsIgnoreCase('JIT')")
     public void onListenDeliveryOrder(BillDTO billDTO) throws InterruptedException {
+        Thread.sleep(5000);
+        log.info("start to delivery JIT dispatch bill ....." + LocalDateTime.now());
+    }
+
+    @EventListener(condition = "#billDTO.billType.equalsIgnoreCase('JIT')")
+    @Order(1)
+    public void onListenDeliveryOrder2(BillDTO billDTO) throws InterruptedException {
+        Thread.sleep(5000);
+        log.info("start to delivery JIT dispatch bill ....." + LocalDateTime.now());
+    }
+
+    @EventListener(condition = "#billDTO.billType.equalsIgnoreCase('JIT')")
+    @Order(2)
+    public void onListenDeliveryOrder3(BillDTO billDTO) throws InterruptedException {
         Thread.sleep(5000);
         log.info("start to delivery JIT dispatch bill ....." + LocalDateTime.now());
     }
@@ -39,9 +56,11 @@ public class EventService {
 
     /**
      * 处理B2C发货单
-     * 事务消息，
+     * 事务隔离的时间发布 发布时间的地方事务执行成功，这个事件才会被执行，如果简监听的地方出现异常，发布事件的地方无法感知到
+     * fallbackExecution 可以指定如果发布事件的地方没有指定 事务 则此事件不会被成功消费
+     *
      */
-    @TransactionalEventListener(condition = "#billDTO.billType.equalsIgnoreCase('B2C')",phase = TransactionPhase.AFTER_COMMIT)
+    @TransactionalEventListener(condition = "#billDTO.billType.equalsIgnoreCase('B2C')",phase = TransactionPhase.AFTER_COMMIT ,fallbackExecution = true)
     public void onListenB2cDeliveryOrder(BillDTO billDTO) {
         log.info("start to deal with b2c delivery order ,but something error...");
     }
